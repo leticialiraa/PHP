8265540dac15186c5384ef102c039ccd55da5443
Letícia Lira <168122252+leticialiraa@users.noreply.github.com>
2024-08-06 17:57:42 -0300

    Add files via upload

---
+++ aula08.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+
+    <form action="recebe-aula08.php" method= "post">
+        <br><label for="nome">Nome: </label><br>
+        <input type="text" name="nome"><br>
+        <label for="sobrenome">Sobrenome: </label><br>
+        <input type="text" name="sobrenome"><br>
+        <input type="submit" volue="Enviar">
+    </form>
+
+</body>
+</html>
