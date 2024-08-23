0a9b9bad40f4d5311fff5e93bda101a77b20081a
Letícia Lira <168122252+leticialiraa@users.noreply.github.com>
2024-08-23 15:02:55 -0300

    Add files via upload

---
+++ aula10.php
@@ -0,0 +1,66 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="aula10.css"/>
+    <title>Formulário Concurso</title>
+</head>
+<body>
+    <h1>Preencha com seus dados:</h1>
+<form action="confirma.php" method="post">
+        <label for="nome">Nome Completo: </label>
+        <input type="text" name="nome"> <br>
+
+        <label for="data">Data de Nascimento: </label>
+        <input type="text" name="data"> <br>
+
+        <label for="sexo">Sexo: </label>
+        <input type="text" name="sexo"> <br>
+
+        <label for="civil">Estado Civil: </label>
+        <input type="text" name="civil"> <br>
+
+        <label for="nac">Nacionalidade: </label>
+        <input type="text" name="nac"> <br>
+
+        <label for="rg">Número do RG: </label>
+        <input type="text" name="rg"> <br>
+
+        <label for="cpf">CPF: </label>
+        <input type="text" name="cpf"> <br>
+
+        <label for="end">Endereço Completo: </label>
+        <input type="text" name="end"> <br>
+
+        <label for="log">Logradouro: </label>
+        <input type="text" name="log"> <br>
+
+        <label for="num">Número: </label>
+        <input type="text" name="num"> <br>
+
+        <label for="comp">Complemento: </label>
+        <input type="text" name="comp"> <br>
+
+        <label for="bairro">Bairro: </label>
+        <input type="text" name="bairro"> <br>
+
+        <label for="cid">Cidade: </label>
+        <input type="text" name="cid"> <br>
+
+        <label for="est">Estado: </label>
+        <input type="text" name="est"> <br>
+
+        <label for="cep">CEP: </label>
+        <input type="text" name="cep"> <br>
+
+        <label for="tel">Telefone de Contato: </label>
+        <input type="text" name="tel"> <br>
+
+        <label for="email">E-mail: </label>
+        <input type="text" name="email"> <br>
+
+        <input type="submit"> <br>
+    </form>
+</body>
+</html>
