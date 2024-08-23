35ebb2db5dc954006d839e93827d5d097c759892
Letícia Lira <168122252+leticialiraa@users.noreply.github.com>
2024-08-23 14:59:58 -0300

    Add files via upload

---
+++ lira3e/aula11.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Calculando IMC</title>
+    <link rel="stylesheet" href="aula11.css"/>
+</head>
+<body>
+<h1> Calcule agora seu IMC! </h1>
+<form action="imc.php" method="post">
+        <label for="peso">Seu peso (em kg): </label>
+        <input type="text" name="peso"> <br>
+        <label for="altura">Sua altura (em m): </label>
+        <input type="text" name="altura"> <br>
+        <input class="submit" type="submit" value="Calcular IMC"> <br>
+        </form>
+
+</body>
+</html>
