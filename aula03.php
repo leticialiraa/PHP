302527513bbd12ab4ab6168c0f03f4235dca3c85
Letícia Lira <168122252+leticialiraa@users.noreply.github.com>
2024-08-23 17:17:52 -0300

    Update aula03.php

--- aula03.php
+++ aula03.php
@@ -3,20 +3,33 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Doc</title>
+    <title>Estrutura de repetição</title>
+    <style>
+        table{
+            border-collapse: collapse;
+        }
+        td{
+            border: 1px solid black;
+        }
+        </style>
 </head>
 <body>
-
-<?php
-for ($x=1; $x<=10; $x++){
-echo "$x <br>";
-};
-
-
-echo"<h1> Tabela de 1 a 10 em PHP </h1>";
-
-
-?>
-    
+    <?php
+    echo"<h1> Estrutura de repetição FOR</h1>";
+    /**
+     * For(inicialização; condição; incremento/decremento){
+     * BLOCO DE CÓDIGOS
+     * }
+     */
+    for($x = 1; $x <= 10; $x++){
+        echo "$x <br>";
+    }
+    echo"<h1>TABELA EM PHP</h1>";
+    echo "<table> <tr>";
+    for($x = 1; $x <= 10; $x++){
+        echo "<td> $x </td>";
+    }
+    echo"</tr> </table>";
+    ?>
 </body>
 </html>
