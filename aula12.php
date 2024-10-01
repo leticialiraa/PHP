eae169a36c09cf344595f47a391ecacafa40907d
Letícia Lira <168122252+leticialiraa@users.noreply.github.com>
2024-10-01 16:39:38 -0300

    Add files via upload

---
+++ aula12.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Aula 12</title>
+    <link rel="stylesheet" href="aula12.css"/>
+</head>
+<body>
+    <h1>Coloque aqui seus dados!</h1>
+<form action="senha.php" method="post">
+        <label for="user">Nome do usuário: </label>
+        <input type="text" name="user"> <br>
+        <label for="senha">Senha: </label>
+        <input type="password" name="senha"> <br>
+        <input class="submit" type="submit" value="Enviar"> <br>
+        </form>
+</body>
+</html>
